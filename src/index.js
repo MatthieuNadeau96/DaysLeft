02288efbce51b419325772db84e85e4fd72ebdaa
Matthieu <matthieunadeau96@gmail.com>
2017-07-24 13:14:02 -0400

    fixed error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 'use strict';
-var Alexa = require("alexa-sdk"); //'amzn1.echo-sdk-ams.app.your-skill-id';
-// var APP_ID = 'amzn1.ask.skill.bcfd6d9c-2ed4-4f8d-8238-04939ea1d036';
+var Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.appId = 'amzn1.ask.skill.fa9070f2-bc03-4778-9d5d-deb9543c773b';
-    alexa.dynamoDBTableName = 'DaysLeft';
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.appId = 'amzn1.ask.skill.fa9070f2-bc03-4778-9d5d-deb9543c773b';
+  alexa.dynamoDBTableName = 'DaysLeft';
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var speechOutput;
-var reprompt = "Hi.";
-var welcomeOutput = "Hello, welcome to your very own, death calculator!" ;
-// var welcomeReprompt = "Say yes to continue or no to stop.";
+var welcomeOutput = "Hello, welcome to your very own death calculator!" +
+" Would you like to know when you will die?";
+var reprompt = "Say yes to continue or no to stop.";
 var DaysLeftIntro = [
   "Cool. ",
   "Great. ",
@@ -25,25 +24,32 @@ var DaysLeftIntro = [
   "Wow...Really? Well, okay then. "
 ];
 
+// var states = {
+//     GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
+//     STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
+// };
 
+// var newSessionHandlers = {
+//     'NewSession': function() {
+//         if(Object.keys(this.attributes).length === 0) {
+//             // this.attributes['endedSessionCount'] = 0;
+//             // this.attributes['gamesPlayed'] = 0;
+//         }
+//         this.handler.state = states.STARTMODE;
+//         this.emit(':ask', welcomeOutput + ' Do you want to calculate your date?',
+//             'Say yes to start the game or no to quit.');
+//           }
+// };
 var handlers = {
   'LaunchRequest': function() {
-        if(Object.keys(this.attributes).length === 0) {
-            this.attributes['endedSessionCount'] = 0;
-            this.attributes['gamesPlayed'] = 0;
-          }
-          this.emit(':ask', welcomeOutput + " You've played "
-          + this.attributes['gamesPlayed'].toString()
-          + " times. Do want to know when you will die?");
-        },
-  'DaysLeftIntent': function () {
-      this.emit(':ask', 'Okay...' + "I'm going to ask you a few questions to understand more about you.");
-      var filledSlots = delegateSlotCollection.call(this);
-      console.log(JSON.stringify(filledSlots));
-      var speechOutput = randomPhrase(DaysLeftIntro);
+    this.emit(':ask', welcomeOutput, reprompt);
+  },
+  'DaysLeftIntent': function() {
+    var filledSlots = delegateSlotCollection.call(this);
+    console.log(JSON.stringify(filledSlots));
+    var speechOutput = randomPhrase(DaysLeftIntro);
 
-      var dateOfBirth=filledSlots.slots.dateOfBirth.value
-      console.log(this.attributes['gamesPlayed']);
+    var dateOfBirth=filledSlots.slots.dateOfBirth.value
       this.attributes['dateOfBirth'] = dateOfBirth;
       // this.attributes["dateOfBirth"];
       var weight=filledSlots.slots.weight.value;
@@ -85,64 +91,67 @@ var handlers = {
       } else {
         speechOutput += ", and you smoke " + smoke + " packs a day";
       };
-      speechOutput += ". You're still a loser though. And judging by your voice you sound like you don't have any friends at all."
+      // speechOutput += ". You're still a loser though. And judging by your voice you sound like you don't have any friends at all."
       this.emit(":tell", speechOutput);
-  },
-  "AMAZON.HelpIntent": function() {
+    },
+    "AMAZON.HelpIntent": function() {
       this.emit(':tell', reprompt);
     },
-  "AMAZON.StopIntent": function() {
+    "AMAZON.StopIntent": function() {
       this.emit(':tell', "Goodbye!");
     },
-  "AMAZON.CancelIntent": function() {
+    "AMAZON.CancelIntent": function() {
       this.emit(':tell', "Goodbye!");
     },
-  'SessionEndedRequest': function () {
+    'Unhandled': function () {
+      console.log("UNHANDLED");
+    },
+    'SessionEndedRequest': function() {
       console.log('session ended!');
-      //this.attributes['endedSessionCount'] += 1;
-      this.emit(":tell", "Goodbye!");
+      this.emit(":tell", "Goodbye");
     }
 };
-function delegateSlotCollection(){
-  console.log("in delegateSlotCollection");
-  console.log("current dialogState: "+this.event.request.dialogState);
-    if (this.event.request.dialogState === "STARTED") {
-      console.log("in Beginning");
-      var updatedIntent=this.event.request.intent;
-      //optionally pre-fill slots: update the intent object with slot values for which
-      //you have defaults, then return Dialog.Delegate with this updated intent
-      // in the updatedIntent property
-      this.emit(":delegate", updatedIntent);
-    } else if (this.event.request.dialogState !== "COMPLETED") {
-      console.log("in not completed");
-      // return a Dialog.Delegate directive with no updatedIntent property.
-      this.emit(":delegate");
-    } else {
-      console.log("in completed");
-      console.log("returning: "+ JSON.stringify(this.event.request.intent));
-      // Dialog is now complete and all required slots should be filled,
-      // so call your normal intent handler.
-      return this.event.request.intent;
-    }
+  function delegateSlotCollection(){
+    console.log("in delegateSlotCollection");
+    console.log("current dialogState: "+this.event.request.dialogState);
+      if (this.event.request.dialogState === "STARTED") {
+        console.log("in Beginning");
+        var updatedIntent=this.event.request.intent;
+        //optionally pre-fill slots: update the intent object with slot values for which
+        //you have defaults, then return Dialog.Delegate with this updated intent
+        // in the updatedIntent property
+        this.emit(":delegate", updatedIntent);
+      } else if (this.event.request.dialogState !== "COMPLETED") {
+        console.log("in not completed");
+        // return a Dialog.Delegate directive with no updatedIntent property.
+        this.emit(":delegate");
+      } else {
+        console.log("in completed");
+        console.log("returning: "+ JSON.stringify(this.event.request.intent));
+        // Dialog is now complete and all required slots should be filled,
+        // so call your normal intent handler.
+        return this.event.request.intent;
+      }
 }
 
 function randomPhrase(array) {
   var i = 0;
   i = Math.floor(Math.random() * array.length);
   return(array[i]);
-
 }
+
 function isSlotValid(request, slotName){
-  var slot = request.intent.slot[slotName];
-  var slotValue;
+        var slot = request.intent.slots[slotName];
+        //console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
+        var slotValue;
 
-  if (slot && slot.value) {
-    slotValue = slot.value.toLowerCase();
-    return slotValue;
-  } else {
-    return false;
-  }
-}
-function unhandled() {
-  console.log("suck my nut");
+        //if we have a slot, get the text and store it into speechOutput
+        if (slot && slot.value) {
+            //we have a value in the slot
+            slotValue = slot.value.toLowerCase();
+            return slotValue;
+        } else {
+            //we didn't get a value in the slot.
+            return false;
+        }
 }
