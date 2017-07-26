a748f4c03d6f3186eb2144f393cfdbc6b843a733
Matthieu <matthieunadeau96@gmail.com>
2017-07-26 12:36:31 -0400

    more conditions and changed welcomeoutput

--- src/index.js
+++ src/index.js
@@ -10,18 +10,17 @@ exports.handler = function(event, context, callback) {
 };
 
 var speechOutput;
-var welcomeOutput = "Hello, welcome to your very own death calculator!" +
-" Would you like to know when you will die?";
-var reprompt = "Say yes to continue or no to stop.";
+var welcomeOutput = "Hello, I am going to begin by asking you a few questions about yourself to calculate how many days you have left to live. "
++ "Tell me to begin when you are ready. ";
+var reprompt;
 var DaysLeftIntro = [
-  "Cool. ",
+  "Okay. ",
   "Great. ",
   "Nice. ",
-  "That's nice. ",
+  "Alright. ",
   "Excellent. ",
-  "Thank you!. ",
-  "Splendid! ",
-  "Wow...Really? Well, okay then. "
+  "Thank you! ",
+  "Splendid! "
 ];
 
 // var states = {
@@ -39,11 +38,11 @@ var DaysLeftIntro = [
 // };
 var handlers = {
   'LaunchRequest': function() {
-    if(Object.keys(this.attributes).length === 0) {
-      this.attributes['endedSessionCount'] = 0;
-      this.attributes['gamesPlayed'] = 0;
-    }
-    this.emit(':ask', welcomeOutput, reprompt);
+    // if(Object.keys(this.attributes).length === 0) {
+    //   this.attributes['endedSessionCount'] = 0;
+    //   this.attributes['gamesPlayed'] = 0;
+    // }
+    this.emit(':ask', welcomeOutput);
   },
   'DaysLeftIntent': function() {
     var filledSlots = delegateSlotCollection.call(this);
@@ -69,12 +68,12 @@ var handlers = {
       var height=filledSlots.slots.height.value;
       this.attributes['height'] = height;
 
-      var yearsLeft = averageYearsLeft;
+      var yearsLeft = 0;
       var today = new Date();
       var currentDate = today.getFullYear() + '-'
       + (today.getMonth()+1) + '-' + today.getDate();
       var age = parseInt(currentDate) - parseInt(dateOfBirth);
-      var averageYearsLeft = Math.round((79 - age)/365);
+      var averageYearsLeft = (yearsLeft) + (Math.round((79 - age)/365));
       var daysLeft = (averageYearsLeft*365);
 
       var bodyMassIndex = (parseInt(weight)*703)/(parseInt(height)*parseInt(height));
@@ -85,58 +84,71 @@ var handlers = {
       speechOutput += "Well, you aren't going to like this. But you have " + daysLeft + " days left to live."
 
       // averageYearsLeft =(79-((C16-B3)/365))
-      // EXERCISE =IF((B8="A"), 1.5, IF(B8="B",3.5,IF(B8="C",4.5, IF(B8="D","-3"))))
-      /* if(parseInt(exercise) >= 2 && parseInt(exercise) <= 6) {
-            yearsLeft += 3;
-          } else if (parseInt(exercise) >= 7) {
-            yearsLeft += 5;
-          } else if (parseInt(exercise) == 1){
-            yearsLeft += 1;
-          } else {
-            yearsLeft += 0;
-          }
-      */
-
-      // STRESS =IF(B9="A","1",IF(B9="B","0",IF(B9="C","-1")))
-      /* if(parseInt(stress) <= 4) {
-            yearsLeft += 1;
-          } else if (parseInt(stress) >= 6 && parseInt(stress) <= 10) {
-            yearsLeft -= 1;
-          } else if (parseInt(stress) == 5) {
-            yearsLeft += 0;
-          }
-      */
-      // ALCOHOL =IF(B10="A","1",IF(B10="B","0",IF(B10="C","-3",IF(B10="D","-4"))))
-      /*
-      */
-      // SMOKING =IF(B11="A","2",IF(B11="B","1",IF(B11="C","-1",IF(B11="D","-4",IF(B11="E","-8")))))
-      // DRIVEACCI =IF(B12="A","1",IF(B12="B","0",IF(B12="C","-4",IF(B12="D","-6",IF(B12="E","-12")))))
-      // DUI
-      // BMI =IF(C14<12,"-15",IF(C14<=24,"0",IF(C14<39,"-3",IF(C14<=42,"-10",IF(C14>42,"-30")))))
       // TOTALYEARSLEFT =(C13+C12+C11+C10+C8+C9+C7+C3)
       // DAYSLEFT =(C20*365)
 
+///////////////////////////////////////////////////////
+      //
+      //                     // exercise condition
+      //
+      // if(parseInt(exercise) >= 2 && parseInt(exercise) <= 6) {
+      //   yearsLeft += 3;
+      // } else if (parseInt(exercise) >= 7) {
+      //   yearsLeft += 5;
+      // } else if (parseInt(exercise) == 1){
+      //   yearsLeft += 1;
+      // } else {
+      //   yearsLeft += 0;
+      // };
+      //                     // stress condition
+      // if(parseInt(stress) <= 4) {
+      //   yearsLeft += 1;
+      // } else if (parseInt(stress) >= 6 && parseInt(stress) <= 10) {
+      //   yearsLeft -= 1;
+      // } else if (parseInt(stress) == 5) {
+      //   yearsLeft += 0;
+      // };
+      //
+      // // ALCOHOL =IF(B10="A","1",IF(B10="B","0",IF(B10="C","-3",IF(B10="D","-4"))))
+      // // I think I need to rethink the question for alcohol intake
+      //
+      //                     //smoking condition
+      // if(parseInt(smoke) == 0) {
+      //   yearsLeft += 2;
+      // } else if(parseInt(smoke) >= 2) {
+      //   yearsLeft -= 8;
+      // } else {
+      //   yearsLeft -= 4;
+      // };
+      //
+      //                     //accident condition
+      // if(parseInt(drivingAccident) >= 4) {
+      //   yearsLeft -= 4;
+      // } else if(parseInt(drivingAccident) >= 1) {
+      //   yearsLeft += 0;
+      // } else {
+      //   yearsLeft += 1;
+      // };
+      //                     // DUI condition
+      // if(parseInt(drivingDUI) == 1) {
+      //   yearsLeft -= 6;
+      // } else if (parseInt(drivingDUI) > 1) {
+      //   yearsLeft -= 12;
+      // } else {
+      //   yearsLeft += 1;
+      // };
+      //
+      //
+      // if(bodyMassIndex <= 18.5) {
+      //   yearsLeft -=1;
+      // } else if(bodyMassIndex <= 29) {
+      //   yearsLeft += 0;
+      // } else if(bodyMassIndex <= 39) {
+      //   yearsLeft -= 3;
+      // } else {
+      //   yearsLeft -= 10;
+      // };
 
-
-                          // exercise condition
-
-      if(parseInt(exercise) >= 2 && parseInt(exercise) <= 6) {
-        yearsLeft += 3;
-      } else if (parseInt(exercise) >= 7) {
-        yearsLeft += 5;
-      } else if (parseInt(exercise) == 1){
-        yearsLeft += 1;
-      } else {
-        yearsLeft += 0;
-      };
-                          // stress condition
-      if(parseInt(stress) <= 4) {
-        yearsLeft += 1;
-      } else if (parseInt(stress) >= 6 && parseInt(stress) <= 10) {
-        yearsLeft -= 1;
-      } else if (parseInt(stress) == 5) {
-        yearsLeft += 0;
-      };
       // if (stress == '3' || '2' || '1') {
       //   speechOutput += ', you deal with stress pretty well';
       // } else if (stress == '6' || '7' || '8' || '9' || '10') {
