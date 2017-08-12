b6d2e0fc9b7a9607345973812b32244814c035ea
Matthieu <matthieunadeau96@gmail.com>
2017-08-12 12:21:11 -0400

    Pulls Random Tip and Cleaned up some Code

--- src/index.js
+++ src/index.js
@@ -11,44 +11,20 @@ exports.handler = function(event, context, callback) {
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
-                        // query dynamodb table tips
-
-// function readDynamoItem(params, callback) {
-//
-// var AWS = require('aws-sdk');
-// var docClient = new AWS.DynamoDb.DocumentClient({region: 'us-east-1'});
-//
-// console.log('reading item from DynamoDB table');
-//
-// docClient.get(params, (err, data) => {
-//  if(err) {
-//    console.error("Unable to read item. Error JSON:", JSON.stringify(err,null,2));
-//  } else {
-//    console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-//
-//    callback(data.Item.message);
-//
-//  }
-// });
-//}
 AWS.config.update({
   region: "us-east-1"
-  // endpoint: "http://localhost:8000"
-  // accessKeyId: "",
-  //secretAccessKey: ""
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 function readItem(obj, callback) {
   var table = "Tips";
-  var id = "1";
+  var id = getRandomTip(1, 12).toString();
   var params = {
     TableName: table,
     Key:{
       "Id": id
     }
-    // console.log("PARAMS: " + params);
   };
   docClient.get(params, function(err, data) {
     if(err) {
@@ -56,16 +32,9 @@ function readItem(obj, callback) {
     } else {
       console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
       callback(obj, data['Item']['tip']);
-      // callback(data.Item.tip);  // this particular row has an attribute called message
     }
   });
-  // console.log("IDK: " + docClient.get(params, function(err, data)));
 }
-
-// var params = {
-//   TableName: 'Tips',
-//   Key:{ "Id": 1 }
-// };
 
 var speechOutput;
 var welcomeOutput = "Hello, I am going to begin by asking you a few questions about yourself to calculate how many days you have left to live. "
@@ -80,35 +49,6 @@ var DaysLeftIntro = [
   "Thank you! ",
   "Splendid! "
 ];
-
-// var tip = [
-//   "Walking thirty minutes a day will add about two point two years to your life. That's an extra nine hundred and three more days! ",
-//   "Getting a full one hundred minutes of movement a day will give you about five point six more years on earth. That's another two thousand days! ",
-//   "If you just go to bed 15 minutes earlier than you normally do, you'll add an extra three years to your life. That's about one thousand and ninety five days! ",
-//   "If you eat a more healthy diet you can add up to fourteen years onto your life! That's five thousand one hundred and ten more days on planet earth. ",
-//   "Smoking is extremely bad for you. I'm sure you already know that. If you quit smoking you can add from anywhere between six to twelve years to your life. That's somewhere between two thousand and four thousand days! ",
-//   "Frequent sex will add about three more years to your life. That's another one thousand and ninety five days. ",
-//   "Don't use too much sugar. A high sugar diet boosts blood sugar, which is bad for your heart by increasing levels of LDL cholesterol and tripling your risk for fatal cardiovascular disease. ",
-//   "You should consider extra vitamin D. Vitamin D is a byproduct of sunlight and has many healthy benefits. Though, too much is just as bad as too little. You should ask your doctor if you would benefit from extra D in pill form. ",
-//   "If you've been looking to kick your coffee habit, green tea is a great solution. Green tea contains powerful antioxidants known as catechins. In a large study of more than forty thousand men and woman, drinking five or more cups of green tea a day was associated with a twelve percent decrease in mortality among men and a twenty three degrease among woman. ",
-//   "Eating three or more servings of whole grains each day can cut your overall death rate by about twenty percent. ",
-//   "Drink more water! Staying adequately hydrated can prolong a healthy life by reducing the risk of bladder and colon cancer and keeping your kidneys in great shape. It might also be helpful in eating less and losing weight. ",
-//   "People who drink three to five cups of coffee per day had a fifteen percent lower risk of mortality compared to people who didn't drink coffee. Coffee reduces your risk of stroke, diabetes and some cancers. "
-// // ];
-
-// var states = {
-//     GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
-//     STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
-// };
-
-// var newSessionHandlers = {
-//     'NewSession': function() {
-//         if(Object.keys(this.attributes).length === 0) {
-//             this.attributes['endedSessionCount'] = 0;
-//             this.attributes['gamesPlayed'] = 0;
-//         }
-//       }
-// };
 
 var handlers = {
   'LaunchRequest': function() {
@@ -169,12 +109,6 @@ var handlers = {
       var age = parseInt(currentDate) - parseInt(dateOfBirth);
       var bodyMassIndex = (parseInt(weight)*703)/(parseInt(height)*parseInt(height));
 
-      // averageYearsLeft =(79-((C16-B3)/365))
-      // TOTALYEARSLEFT =(C13+C12+C11+C10+C8+C9+C7+C3)
-      // DAYSLEFT =(C20*365)
-
-///////////////////////////////////////////////////////
-      //
                         // exercise condition
 
       if(parseInt(exercise) >= 2 && parseInt(exercise) <= 6) {
@@ -194,9 +128,6 @@ var handlers = {
       } else if (parseInt(stress) == 5) {
         yearsLeft += 0;
       };
-
-      // ALCOHOL =IF(B10="A","1",IF(B10="B","0",IF(B10="C","-3",IF(B10="D","-4"))))
-      // I think I need to rethink the question for alcohol intake
 
                           //smoking condition
       if(parseInt(smoke) == 0 || smoke == "none" || smoke == "i don't smoke") {
@@ -278,52 +209,6 @@ var handlers = {
 
         obj.emit(":tell", speechOutput += data);
       });
-      //speechOutput += readItem();
-      //console.log("ADITEM: " + readItem());
-
-      // readDynamoItem(params, tip=>{
-      //   var say = '';
-      //
-      //   say = tip;
-      //
-      //   say = ' Your tip is, ' + tip + ". ";
-      //
-      //   this.emit(':tell', say);
-      //
-      // });
-      // speechOutput += randomPhrase(tip);
-
-
-
-      // if (stress == '3' || '2' || '1') {
-      //   speechOutput += ', you deal with stress pretty well';
-      // } else if (stress == '6' || '7' || '8' || '9' || '10') {
-      //   speechOutput += ", you don't do that well with stress";
-      // } else {
-      //   speechOutput += ", you handle stress very well";
-      // };
-      //                   car accident condition
-      // if (drivingAccident == "none" || "I've never been in an accident" || "I've never been in a car accidnt" || 0) {
-      //   speechOutput += ", you haven't been in any car accidents in the past three years";
-      // } else {
-      //   speechOutput += ", you've been in " + drivingAccident + " car accidents in the past three years";
-      // };
-      //                   DUI condition
-      // if (drivingDUI == "none" || "I never drive under the influence" || 0) {
-      //   speechOutput += ", you haven't had any DUI's";
-      // } else {
-      //   speechOutput += ", you've had " + drivingDUI + " DUI's";
-      // };
-      //                   smoking condition
-      // if (smoke == 'none' || "I don't smoke" || 0) {
-      //   speechOutput += ", and you don't smoke";
-      // } else {
-      //   speechOutput += ", and you smoke " + smoke + " packs a day";
-      // };
-
-
-      // speechOutput += ". You're still a loser though. And judging by your voice you sound like you don't have any friends at all."
-      //this.emit(":tell", speechOutput);
     },
     "AMAZON.HelpIntent": function() {
       this.emit(':tell', reprompt);
@@ -371,6 +256,10 @@ function randomPhrase(array) {
   var i = 0;
   i = Math.floor(Math.random() * array.length);
   return(array[i]);
+}
+
+function getRandomTip(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function isSlotValid(request, slotName){
